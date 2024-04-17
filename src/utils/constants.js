ca046941538448e2cf984bdba5b1dbfe78ed49cc
Salman Ahmed <ahmedpec1005@gmail.com>
2024-04-17 21:02:07 +0530

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,23 +23,36 @@
 // export const SEARCH_RESULT =
 //   "https://www.swiggy.com/dapi/restaurants/search/v3?";
 
-export const PLACE_SUGGESTION_API = "/dapi/misc/place-autocomplete?input=";
+export const PLACE_SUGGESTION_API =
+  "/dapi/misc/place-autocomplete?input=" ||
+  "https://www.swiggy.com/dapi/misc/place-autocomplete?input=";
 
-export const PRE_SEARCH_API = "/dapi/landing/PRE_SEARCH?";
+export const PRE_SEARCH_API =
+  "/dapi/landing/PRE_SEARCH?" ||
+  "https://www.swiggy.com/dapi/landing/PRE_SEARCH?";
 
-export const SEARCH_API = "/dapi/restaurants/search/suggest?";
+export const SEARCH_API =
+  "/dapi/restaurants/search/suggest?" ||
+  "https://www.swiggy.com/dapi/restaurants/search/suggest?";
 
 export const CUISINE_IMAGE =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/";
 
-export const COORDINATES = "/dapi/misc/address-recommend?place_id=";
+export const COORDINATES =
+  "/dapi/misc/address-recommend?place_id=" ||
+  "https://www.swiggy.com/dapi/misc/address-recommend?place_id=";
 
-export const HOME_API = "/dapi/restaurants/list/v5?";
+export const HOME_API =
+  "/dapi/restaurants/list/v5?" ||
+  "https://www.swiggy.com/dapi/restaurants/list/v5?";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const MENU_API =
-  "/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=";
+  "/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=" ||
+  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=";
 
-export const SEARCH_RESULT = "/dapi/restaurants/search/v3?";
+export const SEARCH_RESULT =
+  "/dapi/restaurants/search/v3?" ||
+  "https://www.swiggy.com/dapi/restaurants/search/v3?";
